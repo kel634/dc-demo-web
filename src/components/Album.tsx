d49c97bc75d9d239c7ac6bf0e72d2da7ee9ad883
Imre Kelemen <kel634@yahoo.com>
2020-06-04 17:20:34 +0300

    Added dropzone. Cleaned the App component. Added fixed npm versions.

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -3,22 +3,59 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AssetCard from './AssetCard';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { DropzoneDialog, FileObject } from 'material-ui-dropzone';
 
 const useStyles = makeStyles((theme) => ({
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-  }));
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  toolbar: {
+    justifyContent: 'space-between'
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+}));
 
 export default function Album() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let uploadedFiles: FileObject[] = [];
 
   return (
     <React.Fragment>
       <main>
+        <AppBar position="relative">
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap>
+              Album layout
+            </Typography>
+            <Button variant="contained" color="default" startIcon={<CloudUploadIcon />}
+              onClick={() => setOpen(true)}>
+              Upload
+            </Button>
+
+            <DropzoneDialog
+              acceptedFiles={['image/*']}
+              cancelButtonText={"cancel"}
+              submitButtonText={"submit"}
+              maxFileSize={5000000}
+              open={open}
+              fileObjects={uploadedFiles}
+              onClose={() => setOpen(false)}
+              onSave={(files: any) => {
+                console.log('Files:', files);
+                setOpen(false);
+              }}
+              showPreviews={true}
+              showFileNamesInPreview={true}
+            />
+          </Toolbar>
+        </AppBar>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
             {cards.map((card) => (
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Footer from './Footer';
 import Album from './Album'
 
@@ -11,13 +8,6 @@ export default function App() {
   return (
     <Container>
       <Box my={4}>
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" noWrap>
-              Album layout
-            </Typography>
-          </Toolbar>
-        </AppBar>
         <Album />
         <Footer />
       </Box>
