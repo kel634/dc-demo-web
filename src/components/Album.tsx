73d1f5ae5c67b3d9b3f8aeca9ec3ba3a3e50b71f
Imre Kelemen <kel634@yahoo.com>
2020-06-04 17:35:11 +0300

    Added drawer (folder tree)

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -6,9 +7,13 @@ import AssetCard from './AssetCard';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Button } from '@material-ui/core';
+import { Button, IconButton, Drawer, Divider, ListItem, ListItemText, List } from '@material-ui/core';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { DropzoneDialog, FileObject } from 'material-ui-dropzone';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -18,44 +23,103 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
+  drawerMenuButton: {
+    marginRight: theme.spacing(2),
+  },
+  drawerHide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
 }));
 
 export default function Album() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [dialogOpen, dialogSetOpen] = React.useState(false);
   const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let uploadedFiles: FileObject[] = [];
 
+  const [drawerOpen, drawerSetOpen] = React.useState(false);
+  const handleDrawerOpen = () => {
+    drawerSetOpen(true);
+  };
+  const handleDrawerClose = () => {
+    drawerSetOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <main>
-        <AppBar position="relative">
-          <Toolbar className={classes.toolbar}>
-            <Typography variant="h6" color="inherit" noWrap>
-              Album layout
+      <AppBar position="relative">
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.drawerMenuButton, dialogOpen && classes.drawerHide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
             </Typography>
-            <Button variant="contained" color="default" startIcon={<CloudUploadIcon />}
-              onClick={() => setOpen(true)}>
-              Upload
+          <Button variant="contained" color="default" startIcon={<CloudUploadIcon />}
+            onClick={() => dialogSetOpen(true)}>
+            Upload
             </Button>
 
-            <DropzoneDialog
-              acceptedFiles={['image/*']}
-              cancelButtonText={"cancel"}
-              submitButtonText={"submit"}
-              maxFileSize={5000000}
-              open={open}
-              fileObjects={uploadedFiles}
-              onClose={() => setOpen(false)}
-              onSave={(files: any) => {
-                console.log('Files:', files);
-                setOpen(false);
-              }}
-              showPreviews={true}
-              showFileNamesInPreview={true}
-            />
-          </Toolbar>
-        </AppBar>
+          <DropzoneDialog
+            acceptedFiles={['image/*']}
+            cancelButtonText={"cancel"}
+            submitButtonText={"submit"}
+            maxFileSize={5000000}
+            open={dialogOpen}
+            fileObjects={uploadedFiles}
+            onClose={() => dialogSetOpen(false)}
+            onSave={(files: any) => {
+              console.log('Files:', files);
+              dialogSetOpen(false);
+            }}
+            showPreviews={true}
+            showFileNamesInPreview={true}
+          />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={drawerOpen}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+            <ListItem button key={1}>
+              <ListItemText primary="test" />
+            </ListItem>
+        </List>
+      </Drawer>
+      <main>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
             {cards.map((card) => (
